738196c25ace56543ad939c50084645a33b8cb8b
Alina Proskurnitska <proskur1987@gmail.com>
2022-09-30 15:26:00 +0300

    add preloader

--- js/preloader.js
+++ js/preloader.js
@@ -1,18 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const mediaFile = document.querySelectorAll('img, video');
-    let i = 0
+// document.addEventListener('DOMContentLoaded', () => {
+//     const mediaFile = document.querySelectorAll('img, video');
+//     let i = 0
     
 
-    Array.from(mediaFile).forEach((file, i) => {
-        file.onload = () => {
-            i += 1
+//     Array.from(mediaFile).forEach((file, i) => {
+//         file.onload = () => {
+//             i += 1
             
-             percents.innerHTML = ((i * 100) / mediaFile.length).toFixed()
+//              percents.innerHTML = ((i * 100) / mediaFile.length).toFixed()
 
-            if (i === mediaFile.length) {
-                preloader.classList.add('preloader_hide')
-                percents.innerHTML = 100
-            }
-        }
-    })
-})
+//             if (i === mediaFile.length) {
+//                 preloader.classList.add('preloader_hide')
+//                 percents.innerHTML = 100
+//             }
+//         }
+//     })
+// })
+
+
+
+export default document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('preloader--hide');
+    setTimeout(() => {
+        preloader.remove();
+    }, 1000);
+});
+
