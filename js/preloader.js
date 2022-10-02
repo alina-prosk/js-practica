41fa2200ae23cd9593a2003db84c67752b90a637
Alina Proskurnitska <proskur1987@gmail.com>
2022-10-02 21:20:23 +0300

    апва

--- js/preloader.js
+++ js/preloader.js
@@ -1,30 +1,36 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const mediaFile = document.querySelectorAll('img, video');
-//     let i = 0
-    
-
-//     Array.from(mediaFile).forEach((file, i) => {
-//         file.onload = () => {
-//             i += 1
-            
-//              percents.innerHTML = ((i * 100) / mediaFile.length).toFixed()
-
-//             if (i === mediaFile.length) {
-//                 preloader.classList.add('preloader_hide')
-//                 percents.innerHTML = 100
-//             }
-//         }
-//     })
-// })
 
 
 
-export default document.querySelector('.preloader');
+// export default document.querySelector('.preloader');
 
-window.addEventListener('load', () => {
-    preloader.classList.add('preloader--hide');
-    setTimeout(() => {
-        preloader.remove();
-    }, 1000);
-});
+// window.addEventListener('load', () => {
+//     preloader.classList.add('preloader--hide');
+//     setTimeout(() => {
+//         preloader.remove();
+//     }, 1000);
+// });
+
+
+
+ const loaderFromModal = document.querySelector('.preloader');
+
+// // window.addEventListener('load', () => {
+// //     preloader.classList.add('preloader--hide');
+// //     setTimeout(() => {
+// //         preloader.remove();
+// //     }, 1000);
+// // });
+
+// function hideLoaderFromModal() {
+//     loaderFromModal.classList.add('preloader--hide');
+// }
+
+// function showLoaderFromModal() {
+//     loaderFromModal.classList.remove('preloader--hide');
+// }
+
+//  setTimeout(() => {
+//      hideLoaderFromModal();
+//     }, 1000);
+
 
